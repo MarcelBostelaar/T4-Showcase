4a35b84cfdd82bd0d6ca040b1b08b35b16fd9b09
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2017-07-25 23:10:55 +0200

    Added showcase

---
+++ T4 Showcase/T4 Showcase/ExampleTemplate.cs
@@ -0,0 +1,54 @@
+﻿
+/*
+**This tuplecode was generated using a text template.
+*/
+
+
+class TupleOf2<Type1,Type2>{
+	public TupleOf2(Type1 item1,Type2 item2){
+		Item1 = item1;
+		Item2 = item2;
+	}
+	public Type1 Item1{get; private set;}
+	public Type2 Item2{get; private set;}
+}
+
+class TupleOf3<Type1,Type2,Type3>{
+	public TupleOf3(Type1 item1,Type2 item2,Type3 item3){
+		Item1 = item1;
+		Item2 = item2;
+		Item3 = item3;
+	}
+	public Type1 Item1{get; private set;}
+	public Type2 Item2{get; private set;}
+	public Type3 Item3{get; private set;}
+}
+
+class TupleOf4<Type1,Type2,Type3,Type4>{
+	public TupleOf4(Type1 item1,Type2 item2,Type3 item3,Type4 item4){
+		Item1 = item1;
+		Item2 = item2;
+		Item3 = item3;
+		Item4 = item4;
+	}
+	public Type1 Item1{get; private set;}
+	public Type2 Item2{get; private set;}
+	public Type3 Item3{get; private set;}
+	public Type4 Item4{get; private set;}
+}
+
+class TupleOf5<Type1,Type2,Type3,Type4,Type5>{
+	public TupleOf5(Type1 item1,Type2 item2,Type3 item3,Type4 item4,Type5 item5){
+		Item1 = item1;
+		Item2 = item2;
+		Item3 = item3;
+		Item4 = item4;
+		Item5 = item5;
+	}
+	public Type1 Item1{get; private set;}
+	public Type2 Item2{get; private set;}
+	public Type3 Item3{get; private set;}
+	public Type4 Item4{get; private set;}
+	public Type5 Item5{get; private set;}
+}
+
---
+++ T4 Showcase/T4 Showcase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4_Showcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //See ExampleTemplate.tt
+            TupleOf5<int, int, int, int, int> example = new TupleOf5<int, int, int, int, int>(1, 2, 3, 4, 5);
+        }
+    }
+}
